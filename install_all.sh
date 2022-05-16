e0a482bcdfe1dfd0b11a5109b76dbb93896ead6a
Neel <neel783d@gmail.com>
2022-05-16 19:42:08 +0900

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -14,3 +14,14 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 cp ~/.vim/bundle/gruvbox/colors/gruvbox.vim ~/.vim/colors/
 vim +PluginInstall +qall
+
+
+echo "git username and password store.."
+cd ./setup-linux/
+git config --global user.email "neel783d@gmail.com"
+git config --global user.name "neel783d"
+git config credential.helper store
+git push origin HEAD
+cd ..
+
+echo "update your email to org email"
